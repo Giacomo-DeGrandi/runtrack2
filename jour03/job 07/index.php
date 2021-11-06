7ec8970e21c7238d45411a4267b07c27e2efcf56
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-06 21:40:23 +0100

    jour 3 job 7

---
+++ jour03/job 07/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$str = 'Certaines choses changent, et d\'autres ne changeront jamais';
+		// ertaines choses .... jamaiC
+
+$x=+1;
+$z=$x;
+
+
+while(isset($str[$x])) {
+		while ($z=0){
+			echo false;
+		}
+		echo $str[$x];
+		$x++;}
+
+
+echo $str[0];
+
+
+?>
