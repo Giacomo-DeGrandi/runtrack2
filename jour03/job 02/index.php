c5a5d3c7c130d77e33519f169bae19e6334f884d
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-06 21:05:19 +0100

    jour 3 job 02

--- jour03/job 02/index.php
+++ jour03/job 02/index.php
@@ -1,17 +1,22 @@
 <?php
 
+$str = "I m sorry Dave I m afraid I can t do that";
+$str2 = ['a','e','i','o','I'];
 
-$str = 'Tous ces instants seront perdus dans le temps comme les larmes sous la pluie';
-$num = 0 ;
+$i = 0;
+$x = 0;
+  
+while (isset($str[$i])) {
+	foreach ($str2 as $x){
+		if($str[$i] == $x){ 
+			echo $str[$i];
+		}
+		$x++;
 
-while (isset($str[$num]) == TRUE){
-	if (isset($num) == TRUE and $num % 2  == 0 ){
-		echo $str[$num];
-	} else {
-		echo false;
 	}
-	$num++;
+	$i++;
 }
+
 
 
 ?>
