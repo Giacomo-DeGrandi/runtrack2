73b676d3bf768a767e34f698d31048eaf0c301e1
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-11-17 14:37:26 +0100

    jour 9 finit

---
+++ jour09/job02/index.php
@@ -0,0 +1,46 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'jour08';
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+																		// Template to create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+$req = $conn->query("SELECT nom,capacite FROM salles");
+
+$res = mysqli_fetch_all($req, MYSQLI_ASSOC);  // MERCI NICO!!!
+
+?>
+
+<html>
+<style>
+	table td{
+		border: solid 1px black;
+	}
+</style>
+
+<table><tr>
+
+<?php
+
+
+foreach($res[0] as $k => $v){
+	echo '<td>'. $k . '</td>';
+}
+
+
+foreach ($res as $k2 => $v2){
+	echo '<tr>';
+	foreach($v2 as $k3 => $v3){
+		echo '<td>'. $v3 .' '. '</td>';
+		}
+	echo '</tr>';
+}
+echo '</table>';
+
+?>
+
+</html>
